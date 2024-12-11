88de40c67541f8ec017a45cf483e84e633a04f77
Grigory Tupikov <gtupikov@jetlend.ru>
2024-12-11 20:53:23 +0300

    remove tables

--- utils.py
+++ utils.py
@@ -6,21 +6,6 @@ def is_valid_number(input_text):
         return False
 
 
-def format_payment_table(payments):
-    # Заголовок таблицы
-    table = " Месяц |   Платеж    |   Проценты  | Основной долг | Остаток долга\n"
-    table += "-" * 66 + "\n"
-    
-    # Заполняем строки таблицы данными
-    for payment in payments:
-        table += f"{payment['month']:>6} | " \
-                 f"{payment['payment']:>11,.2f} | " \
-                 f"{payment['interest_payment']:>11,.2f} | " \
-                 f"{payment['principal_payment']:>13,.2f} | " \
-                 f"{payment['remaining_balance']:>13,.2f}\n"
-    
-    return table.replace(",", " ")  # Меняем запятые на пробелы для русской локали
-
 def format_payment_for_message(payments):
     """Форматируем список платежей для отправки в сообщение."""
     # Заголовок таблицы
